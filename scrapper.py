c6347105656f75ca67c2444a3a8742395c001f4d
Joseph <dev@ravimoshark.com>
2020-01-22 22:18:28 +0100

    scrapper non social

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from threading import Thread
+from urllib.request import urlopen
+import re
 
 USER_AGENT = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
@@ -36,6 +39,86 @@ def parse_results(html, keyword):
                 rank += 1
     return found_results
 
+def extract_domain(links):
+    regex = r"^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/?\n]+)"
+    results = re.findall(regex,','.join(links))
+    domains=[]
+    if results :
+        domains = [domain.group(1) for domain in results]
+    return domains
+
+class ThreaderDomainExplorer(Thread):
+    """scrap a webpage looking for string corresponding to
+    a regex and return all links found within the same domain
+    
+    Arguments:
+        Thread {[type]} -- [description]
+    
+    Returns:
+        [tuple] -- [two lists containing respectively links, and codes]
+    """
+
+    def __init__(self, domain ,url, user_agent, code_regex):
+        """[summary]
+        
+        Arguments:
+            domain {[type]} -- [description]
+            url {[type]} -- [description]
+            user_agent {[type]} -- [description]
+            code_regex {[type]} -- [description]
+        """
+        Thread.__init__(self)
+        self.url = url
+        self.user_agent = user_agent
+        self.links=[]
+        self.codes=[]
+        self.domain=domain
+        self.regex = r''+domain
+        self.code_regex = code_regex
+        self.page 
+
+    def get_data(self):
+        """[summary]
+        
+        Returns:
+            [type] -- [description]
+        """
+        if re.match(self.regex, self.url, re.IGNORECASE):
+            response = requests.get(self.url, headers=self.user_agent)
+            response.raise_for_status()
+            self.page = response.text
+        return self.page
+    
+
+    def get_links(self):
+        """get all links of the webpage within the same domain
+        
+        Arguments:
+            soup {[type]} -- [description]
+        """
+        soup = BeautifulSoup(self.page, 'html.parser')
+        for link in soup.findAll('a', attrs={'href': re.compile("^http?s://")}):
+            if self.domain in link:
+                self.links.append(link.get('href'))
+        return self.links
+
+    def get_code(self):
+        """[summary]
+        """
+        self.codes = re.findall(self.code_regex,self.page)
+        return self.codes
+
+    
+    def run(self):
+        """'[summary]'
+        """
+        self.get_data()
+        self.get_code()
+        self.get_links()
+        return True
+
+
+    
 
 if __name__ == '__main__':
     keyword, html = get_google_search_results('SHARETHELOVE*+LUKO', 100, 'en')
