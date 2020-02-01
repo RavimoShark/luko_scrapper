9ee9b41f9b9d9b74eea1198bd5d0c791331abab1
Joseph <dev@ravimoshark.com>
2020-02-01 01:13:45 +0100

    To Do loop

--- scrapper.py
+++ scrapper.py
@@ -135,18 +135,18 @@ async def write_one(file_res:IO, url: str, domain:str, urls_found:set, codes_fou
         return None
     async with aiofiles.open(file_res, "a") as f:
         for u_found,c_found in zip(res[0], res[1]):
-            await f.write(f"{datetime.datetime.now().timestamp()}\t{url}\t{domain}\t{u_found}\t{c_found}\t{0}\n")
+            await f.write(f"{datetime.datetime.now().timestamp()};##;{url};##;{domain};##;{u_found};##;{c_found};##;{False}\n")
             new_elt_per_domain[domain]['url'] = new_elt_per_domain[domain].get('url',0)+1
             new_elt_per_domain[domain]['code'] = new_elt_per_domain[domain].get('code',0)+1
             count += 1
         if len(res[0]) > len(res[1]):
             for u_found  in res[0][count:] : 
-                await f.write(f"{datetime.datetime.now().timestamp()}\t{url}\t{domain}\t{u_found}\t{'Not Found'}\t{0}\n")
+                await f.write(f"{datetime.datetime.now().timestamp()};##;{url};##;{domain};##;{u_found};##;;##;{False}\n")
                 new_elt_per_domain[domain]['url'] = new_elt_per_domain[domain].get('url',0)+1
                 count += 1
         elif len(res[0])<len(res[1]):
              for c_found in res[1][count:]: 
-                await f.write(f"{datetime.datetime.now().timestamp()}\t{url}\t{domain}\t{'Not Found'}\t{c_found}\t{0}\n")
+                await f.write(f"{datetime.datetime.now().timestamp()};##;{url};##;{domain};##;;##;{c_found};##;{False}\n")
                 new_elt_per_domain[domain]['code'] = new_elt_per_domain[domain].get('code',0)+1
                 count += 1
     logger.info("Wrote results for code found: %s for url: %s", new_elt_per_domain[domain].get('code',0), domain)
@@ -154,51 +154,55 @@ async def write_one(file_res:IO, url: str, domain:str, urls_found:set, codes_fou
     
     return new_elt_per_domain    
 
-async def bulk_crawl_and_write(file_res: IO, sel_data: list, urls_found: set, codes_found: set, **kwargs) -> None:
+async def bulk_crawl_and_write(file_res: IO, sel_data: dict, urls_found: set, codes_found: set, **kwargs) -> None:
     """Crawl & write concurrently to `file` for multiple `urls`."""
-    async with ClientSession() as session:
+    async with ClientSession(read_timeout=120) as session:
         tasks = []
         resultat = {}
+        fetch_url=[]
         for domain, urls in sel_data.items():
             logger.info("urls input %s", urls)
             if domain in RESTRICTED_DOMAINS:
                 continue
             for url in urls:
+                fetch_url.append(url)
                 tasks.append(write_one(file_res=file_res, url=url, domain=domain,
                  session=session, urls_found = urls_found, codes_found=codes_found, **kwargs))
         res = await asyncio.gather(*tasks)
         for domain, _ in sel_data.items():
             resultat[domain] = {'url' :np.sum([d[domain].get('url',0) for d in res if d.get(domain)]),
              'code':np.sum([d[domain].get('code',0) for d in res if d.get(domain)])}
-    return resultat
+        res = process_batch_res(set(fetch_url), resultat, file_res)
+    return res
 
 def process_batch_res(fetch_url:set, new_elt_per_domain:dict, res_file, url_count=100):
     
-    data= pd.read_csv(res_file.resolve(), sep='/t', header=0)
-    data.loc[data['parsed_url'].isin(fetch_url),'processed'] = 1
+    data= pd.read_csv(res_file.resolve(), sep=';##;', header=0, engine='python')
+    data.loc[data['parsed_url'].isin(list(fetch_url)),'processed'] = True
     codes_found = data['code'].unique()
-    urls_found = data['url_parsed'].unique()
+    urls_found = data['parsed_url'].unique()
     domains_url = {}
-
-    
     for domain in new_elt_per_domain.keys():
         if url_count <=0 :
             break
         if new_elt_per_domain[domain].get('url',0) > 0:
-            expl = data[(data['code'].isnull()) & (data['domain']==domain) & (data['processed']==1)].shape[0]
+            expl = data[(data['code'].isnull()) & (data['domain']==domain) & (data['processed'])].shape[0]
             if expl> EXPL_LIMITS:
                 logger.info('The domain %s has reached his exploration limits %d links without any new codes', domain,EXPL_LIMITS)
                 continue
-            urls = data.loc[(data['domain']==domain) & (data['processed']==0),'parsed_url'].unique()
+            urls = list(data.loc[(data['domain']==domain) & (~data['processed']),'parsed_url'].unique())
             if not urls:
-                data.loc[data['domain']==domain,'processed'] = 1
+                data.loc[data['domain']==domain,'processed'] = True
                 logger.info('The domain %s has been entirely processed', domain)
                 continue
             else :
                 limit = min(url_count,20,len(urls))
-                elt = np.random.randint(1,limit)
+                if limit <=1:
+                    elt=1
+                else:
+                    elt = np.random.randint(1,limit)
                 url_count-= elt
-                domains_url[domain] = list(np.random.choice(urls,url_count,replace=False))
+                domains_url[domain] = list(np.random.choice(urls,elt,replace=False))
 
     return urls_found, codes_found, domains_url
 
@@ -207,13 +211,12 @@ def process_batch_res(fetch_url:set, new_elt_per_domain:dict, res_file, url_coun
 if __name__ == '__main__':
     keyword, html = get_google_search_results('SHARETHELOVE*+LUKO', 100, 'fr')
     found_results = parse_results(html, keyword)
-    sel_data = pd.Series(found_results[~found_results.domain.isin(RESTRICTED_DOMAINS)].groupby('domain')['link'].first()).to_dict()
-    sel_data ={k:[v] for k,v in sel_data.items()}
+    sel_data = sel_data = pd.Series(found_results.groupby('domain')['link'].agg(lambda x :list(x.values))).to_dict()
     assert sys.version_info >= (3, 7), "Script requires Python 3.7+."
     here = pathlib.Path(__file__).parent
     outpath_res = here.joinpath("res.txt")
     with open(outpath_res, "w") as outfile:
-        outfile.write("timestamp\tsource_url\tdomain\tparsed_url\tcode\tprocessed\n")
+        outfile.write("timestamp;##;source_url;##;domain;##;parsed_url;##;code;##;processed\n")
     resultat = asyncio.run(bulk_crawl_and_write(outpath_res, sel_data,sel_data.values, set())) 
-    res = process_batch_res(sel_data.values, resultat, outpath_res)
-    resultat = asyncio.run(bulk_crawl_and_write(outpath_res, res[2],res[0], res[1])) 
+    res = process_batch_res(set([url for urls in sel_data.values() for url in urls]), resultat, outpath_res)
+    
